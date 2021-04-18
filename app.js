e8ea4bb04eb4a4972c01d7ce157eced3cef54a2f
HMQ23 <hominhquang15026688@gmail.com>
2021-04-18 16:30:55 +0700

    Done!

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
 const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-cafe-form");
 
 // create element & render cafe
 function renderCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "x";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  // deleting data
+  cross.addEventListener("click", (event) => {
+    let id = event.target.parentElement.getAttribute("data-id");
+    db.collection("cafes").doc(id).delete();
+  });
 }
 
 // getting data
-db.collection("cafes")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      renderCafe(doc);
-    });
+// db.collection("cafes")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       renderCafe(doc);
+//     });
+//   });
+
+// saving data
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  db.collection("cafes").add({
+    name: form.name.value,
+    city: form.city.value,
   });
+  form.name.value = "";
+  form.city.value = "";
+});
+
+// real-time listener
+db.collection("cafes").onSnapshot((snapshot) => {
+  let changes = snapshot.docChanges();
+  changes.forEach((change) => {
+    if (change.type == "added") {
+      renderCafe(change.doc);
+    } else if (change.type == "removed") {
+      let li = cafeList.querySelector("[data-id = " + change.doc.id + "]");
+      cafeList.removeChild(li);
+    }
+  });
+});
